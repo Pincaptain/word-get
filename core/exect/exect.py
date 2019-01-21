1b4bb4fb7ad89fea4f404553c42bcc7b71f50529
Akatosh, the God of Time and Order <borjan_blizz@hotmail.com>
2019-01-21 01:41:26 +0100

    Add export feature

--- core/exect/exect.py
+++ core/exect/exect.py
@@ -2,6 +2,7 @@ from core.util import variables
 from core.functions import parser
 from core.functions import requests
 from core.functions import scraper
+from core.functions import exporter
 
 
 def execute(argv):
@@ -70,6 +71,27 @@ def loop():
                 print('Description: {0}'.format(word['description']) if len(word['description']) != 0 else 'Description: (No description provided)')
                 print('Examples: {0}'.format(str(word['examples'])[1: len(str(word['examples'])) - 1]) if len(word['examples']) != 0 else 'Examples: (No examples provided)')
                 print('')
+
+        print('Type JSON, XML, CSV to export the data to the specific format or any other key to continue.')
+        print('')
+
+        export_or_not = input('Export: ')
+
+        if export_or_not == 'JSON':
+            exporter.export_to_json(words, query)
+            print('Export successful. Seach for an output.json file inside your application root directory!')
+        elif export_or_not == 'XML':
+            exporter.export_to_xml(words, query)
+            print('Export successful. Seach for an output.xml file inside your application root directory!')
+        elif export_or_not == 'CSV':
+            exporter.export_to_csv(words, query)
+            print('Export successful. Seach for an output.csv file inside your application root directory!')
+        else:
+            print('')
+
+            continue
+
+        print('')
 
     print('')
     print('Thanks for supporting this program!')
